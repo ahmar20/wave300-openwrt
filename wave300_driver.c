9c41517dcb3237e6756e2bace34f2b22bdfc544c
Ahmar <pakahmar@hotmail.com>
2019-09-02 21:25:41 +0500

    Update initialize procedure

--- wave300_driver.c
+++ wave300_driver.c
@@ -4,6 +4,11 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/pci_regs.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <asm/irq.h>
+#include <pci_vendor_registers.h>
 
 
 #define LOG(string...) printk(KERN_INFO string)
@@ -11,11 +16,20 @@
 #define		WAVE300_Vendor_Id	0x1a30
 #define		WAVE300_Device_Id	0x0700		// WAVE300 8224
 #define		WAVE300_Device_Id_2	0x0710		// WAVE300 8231
+#define		DMA_64BIT_MASK		DMA_BIT_MASK(64)
+#define		DMA_32BIT_MASK		DMA_BIT_MASK(32)
+
 
 #ifndef		COMMANDS
 	static int device_probe(struct pci_dev *dev, const struct pci_device_id *id);
 	static void device_remove(struct pci_dev *dev);
 	static void show_device_info(struct pci_dev *dev);
+	static int reserve_memory_wave300(struct pci_dev *dev);
+	static irqreturn_t wave300_interrupt_handler(int irq, void *dev_id);
+	struct interrupt_info_to_pass 
+	{ 
+		int *some_int; 
+	} dev_info;
 	
 	#ifdef 	OldStylePCIGetDevice
 		static int find_current_wave300_device(void);
@@ -25,6 +39,7 @@
 
 #ifndef		VARIABLES
 	static struct pci_dev *pci_dev_wave300;
+	static int wave300_bar = 0xa0;
 	static struct pci_device_id pci_device_id_table_wave300[] =
 	{
 		{
@@ -56,33 +71,70 @@
 		//resume: device_resume,
 		//suspend: device_suspend,
 	};
+	const char *wave300_resource_name = "wave300_memory_resources";
+	static int irq;
+	const char *interrupt_name = "wave300";
 
-	static unsigned long mmio_base;
-	static unsigned long mmio_length;
-	static unsigned long mmio_flags;
-	static unsigned char wave300_irq;
 #endif
 
-static int 	wave300_driver_init(void)
+static int wave300_driver_init(void)
 {
 	int err;
+	int mwi_enable;
 	LOG("WAVE300 Module initializing...\n");
 	err = pci_register_driver(&pci_driver_wave300);
 	if (err < 0) LOG("Device registration failed with error: %x.\n", err);
 	else
 	{
-		LOG("Device registration succeeded.\n");
+		show_device_info(pci_dev_wave300);
+		pci_set_master(pci_dev_wave300);
+	
+		mwi_enable = pci_try_set_mwi(pci_dev_wave300);
+		if (mwi_enable < 0)
+		{
+			LOG("Memory-Write-Invalidate not possible for WAVE300.\n");
+		}
+		
+		err = pci_request_region(pci_dev_wave300, wave300_bar, wave300_resource_name);
+		if (err < 0)
+		{ 
+			LOG("Failed to acquire memory regions for WAVE300.\n");
+			return err;
+		}
+		else
+		{
+			LOG("Aquired memory regions for WAVE300.\n");
+		}
+		err = pci_set_dma_mask(pci_dev_wave300, DMA_64BIT_MASK);
+		if (err < 0)
+		{
+			LOG("64bit DMA mask failed.\n");
+			err = pci_set_dma_mask(pci_dev_wave300, DMA_32BIT_MASK);
+			if (err < 0)
+			{
+				LOG("32bit DMA mask failed.\n");
+				return err;
+			}
+		}
+		else
+		{
+			LOG("64bit mask set for WAVE300.\n");
+		}
+		// clear any pending interrupts here before registering interrupt handler
+		irq = pci_dev_wave300->irq;
+		enable_irq(irq);
+		err = request_irq(irq, &wave300_interrupt_handler, IRQF_SHARED, interrupt_name, &dev_info);
+		if (err < 0)
+		{ 
+			LOG("IRQ registration failed with error code: %d\n", err);
+			return err;
+		}
+		else
+		{
+			LOG("IRQ registered for WAVE300.\n");
+		}
 	}
 	
-	if (err < 0)
-	{
-		LOG("Initializing failed.\n");
-	}
-	else
-	{
-		LOG("Initializing succeeded.\n");
-	}
-
 	return err;
 }
 
@@ -96,30 +148,27 @@ static void wave300_driver_exit(void)
 static int device_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
 	int err;
-	pci_dev_wave300 = dev;
 
 	LOG("Device probed.\n");
 	err = pci_enable_device(dev);
 	if (err < 0)
 	{
 		LOG("Failed while enabling. Error: %d\n", err);
+		return err;
 	}
-	else
-	{
-		LOG("Device enable succeeded\n");
-	}
-
-	show_device_info(dev);
-
-	//err = pci_request_regions()
-
+	pci_dev_wave300 = dev;
 	return err;
 }
 
 static void device_remove(struct pci_dev *dev)
 {
-	pci_release_regions(dev);
+	disable_irq(irq);
+	free_irq(irq, &dev_info);
+	pci_clear_mwi(dev);
+	pci_clear_master(dev);
+	
 	pci_disable_device(dev);
+	pci_release_region(dev, wave300_bar);
 }
 
 static void show_device_info(struct pci_dev *dev)
@@ -129,6 +178,13 @@ static void show_device_info(struct pci_dev *dev)
 	D3-D0-Transition-Time(%x), \nConfiguration-Space-Size(%x), IRQ(%x)\n",
 	dev->vendor, dev->device, dev->class, dev->current_state, dev->pm_cap, dev->d1_support,
 	dev->d2_support, dev->clear_retrain_link, dev->d3_delay, dev->cfg_size, dev->irq);
+}
+
+static irqreturn_t wave300_interrupt_handler(int irq, void *dev_Id)
+{
+	LOG("IRQ %x may be handled here!\n", irq);
+
+	return IRQ_HANDLED;
 }
 
 #ifdef		OldStylePCIGetDevice
